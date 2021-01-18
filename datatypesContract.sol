b9f1ed48b61a26600a8cdc40814c222babcce6f3
Satyam Srivastava <srivastavasatyam94@gmail.com>
2021-01-18 15:42:51 +0530

    Solidity predefined data types.

---
+++ datatypesContract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.8.0;
+contract myContract{
+    // instead of using a getter function, if we make the state variable public,
+    // we can view the value later.
+    // instead of the constructor, we could initialise a value here.
+    string public stringValue = "myValue";
+    function set(string memory _value) public {
+        stringValue = _value;
+    }
+    // contant keyword makes it a contant
+    string public constant stringValueConstant = "myValue";
+    
+    bool public boolValue = true;
+    int public intValue = -1000;
+    uint public uintValue = 1000; //defaults to uint256
+}
